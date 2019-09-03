24c3c866af67c08c7d76733c2081cd1d8384135f
sheriallis <shrdson@gmail.com>
2019-09-03 21:19:45 +0200

    Update metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Sheri Richardson — Front-End Developer`,
+    description: `Sheri Richardson is a front-end developer based in the Netherlands`,
+    author: `@sheriallis`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
