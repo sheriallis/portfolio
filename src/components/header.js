1f1ff6ffb0277c79cdc9d213f802a38a9245d241
sheriallis <shrdson@gmail.com>
2019-09-03 19:39:42 +0200

    Add header section

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,95 @@
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import styled from "styled-components"
+import { FaCode, FaUserAlt, FaEnvelope } from "react-icons/fa"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
+import breakpoint from "../helpers/breakpoints"
+import siteLogo from "../images/logo_dark.svg"
+import HamburgerIcon from "../images/hamburger_icon.svg"
+
+const StyledHeader = styled.header`
+  background-color: #fdd156;
+  display: flex;
+  justify-content: center;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
+`
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+  width: 100%;
+  max-width: ${breakpoint.maxWidth};
+`
+
+const Logo = styled.img`
+  width: 40px;
+  height: 40px;
+`
+
+const SiteName = styled.h1``
+
+const HamburgerMenu = styled.img`
+  @media ${breakpoint.large} {
+    display: none;
+  }
+`
+const Nav = styled.nav`
+  display: none;
+  @media ${breakpoint.large} {
+    display: block;
+  }
+  ul {
+    list-style: none;
+    display: inline-flex;
+    margin: 0;
+  }
+`
+
+const NavItem = styled.a`
+  padding: 20px;
+  text-decoration: none;
+  color: #222222;
+  font-weight: 600;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  width: 150px;
+
+  svg {
+    margin-right: 16px;
+  }
+`
+
+const Header = () => (
+  <StyledHeader>
+    <Wrapper>
+      <Logo src={siteLogo} alt="site logo" />
+      {/* <SiteName>{siteTitle}</SiteName> */}
+      <HamburgerMenu src={HamburgerIcon} alt="open mobile menu" />
+      <Nav>
+        <ul>
+          <li>
+            <NavItem href="#work">
+              <FaCode />
+              Work
+            </NavItem>
+          </li>
+          <li>
+            <NavItem href="#about">
+              <FaUserAlt /> About
+            </NavItem>
+          </li>
+          <li>
+            <NavItem href="#contact">
+              <FaEnvelope />
+              Contact
+            </NavItem>
+          </li>
+        </ul>
+      </Nav>
+    </Wrapper>
+  </StyledHeader>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
