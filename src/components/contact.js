704b587023aedbeda7d4cb494ec1396489bc7305
sheriallis <shrdson@gmail.com>
2019-09-10 18:42:07 +0200

    Fix form

--- src/components/contact.js
+++ src/components/contact.js
@@ -55,25 +55,29 @@ const Contact = () => (
   <Section id="contact">
     <Title>Get in Touch</Title>
     <FormWrapper>
-      <Form>
-        <label htmlFor="name">
-          Name:
-          <input type="text" id="name" name="name" placeholder="Your name..." />
-        </label>
-        <label htmlFor="email">
-          Your email:
-          <input
-            type="email"
-            id="email"
-            name="email"
-            placeholder="Your email.."
-          />
-        </label>
-        <label htmlFor="message">Your message:</label>
+      <Form name="Contact Form" method="POST" data-netlify="true">
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Your name..."
+          required
+        />
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Your email.."
+          required
+        />
+        <label htmlFor="message">Message</label>
         <textarea
           id="message"
           name="message"
           placeholder="enter your message..."
+          required
         />
         <input type="submit" value="Send message" />
       </Form>
